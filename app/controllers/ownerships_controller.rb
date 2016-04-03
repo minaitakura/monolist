ccc7b4ddd700adab7f4b6ff7157bdd2cbf646359
minaitakura <minaitakura37@gmail.com>
2016-04-03 20:13:10 +0000

    fix Amazon

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,14 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin] , 
+                                  #:search_index => 'All' , 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
+      p "---------＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠-----------------"
+      p response.inspect
+      p "--------------＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠＠------------"
+      #@item = response.item
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
